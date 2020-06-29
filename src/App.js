2ee8e934d78b3373e37cc17f2154557064bd0012
Karem1986 <karem.ortiz@outlook.com>
2020-06-29 13:42:59 +0200

    uploaded awesomeapp2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,208 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div>
+      <header id="header" className="section-header scroll">
+        <a href="./">
+          <img
+            className="icon"
+            src="https://learntocodetogether.nl/assets/icon.svg"
+          />
         </a>
+        <a href="https://www.meetup.com/Learning-to-Code-Amsterdam/">
+          <img
+            className="meetup-icon"
+            src="https://learntocodetogether.nl/assets/meetup-icon.svg"
+          />
+        </a>
+        <nav>
+          <a href="/courses.html">Our Courses</a>
+        </nav>
       </header>
+
+      <div className="section-header-spacer"></div>
+
+      <div className="content">
+        <div className="section section-what-usp">
+          <div className="component-section-header">
+            <div className="title-badge">
+              <div className="title-badge-title">Do you want to</div>
+            </div>
+
+            <h2>
+              <div className="component-title ">Learn how to code</div>
+            </h2>
+
+            <div className="description">
+              Awesome! Let’s do this together. We are here to support you in
+              your coding journey.
+            </div>
+          </div>
+
+          <div className="component-usp-row">
+            <div className="item team">
+              <img
+                className="image"
+                src="https://learntocodetogether.nl/assets/team.svg"
+              />
+              <div className="component-title title">
+                Get help from experienced developers
+              </div>
+            </div>
+            <div className="item team">
+              <img
+                className="image"
+                src="https://learntocodetogether.nl/assets/community.svg"
+              />
+              <div className="component-title title">
+                Learn &amp; share with our community
+              </div>
+            </div>
+            <div className="item team">
+              <img
+                className="image"
+                src="https://learntocodetogether.nl/assets/personal-speed.svg"
+              />
+              <div className="component-title title">
+                Personal &amp; at your own speed
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="section-line">
+          <div className="line "></div>
+        </div>
+
+        <div className="section section-courses-usp">
+          <div className="component-section-header">
+            <div className="title-badge">
+              <div className="title-badge-title">Courses</div>
+            </div>
+
+            <h2>
+              <div className="component-title ">Our Courses</div>
+            </h2>
+
+            <div className="description">
+              We have created courses to help you learn, with easy to follow
+              steps and some sparks of fun!
+            </div>
+          </div>
+
+          <div className="component-usp-row">
+            <div className="item team">
+              <img
+                className="image"
+                src="https://learntocodetogether.nl/assets/expand-horizon.svg"
+              />
+              <div className="component-title title">
+                Learn new skills and expand your horizon
+              </div>
+            </div>
+            <div className="item team">
+              <img
+                className="image"
+                src="https://learntocodetogether.nl/assets/developers.svg"
+              />
+              <div className="component-title title">
+                Follow courses created by experienced developers
+              </div>
+            </div>
+            <div className="item team">
+              <img
+                className="image"
+                style={{ width: "200px" }}
+                src="https://learntocodetogether.nl/assets/step-by-step.svg"
+              />
+              <div className="component-title title">
+                Fun, hands on and easy to follow
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="section-line">
+          <div className="line "></div>
+        </div>
+
+        <div className="section section-why-usp">
+          <div className="component-section-header">
+            <div className="title-badge">
+              <div className="title-badge-title">Why?</div>
+            </div>
+
+            <h2>
+              <div className="component-title ">Why do we do this</div>
+            </h2>
+
+            <div className="description">
+              It’s really simple actually. We just love to combine our passion
+              for code with our love for teaching. Coding is so much fun when
+              doing it together.
+            </div>
+          </div>
+
+          <div className="component-usp-row">
+            <div className="item team">
+              <img
+                className="image"
+                src="https://learntocodetogether.nl/assets/technology.svg"
+              />
+              <div className="component-title title">Technology</div>
+              <div className="description">
+                We love to experiment and create awesome stuff from the comfort
+                of our couch.
+              </div>
+            </div>
+            <div className="item team">
+              <img
+                className="image"
+                src="https://learntocodetogether.nl/assets/community.svg"
+              />
+              <div className="component-title title">People</div>
+              <div className="description">
+                It’s always fun to do things together, the excitement and the
+                collaborations.
+              </div>
+            </div>
+            <div className="item team">
+              <img
+                className="image"
+                src="https://learntocodetogether.nl/assets/personal-speed.svg"
+              />
+              <div className="component-title title">Teaching</div>
+              <div className="description">
+                We like to learn and to teach others the stuff we know.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="section-line">
+        <div className="line "></div>
+      </div>
+
+      <div className="section-footer">
+        <img
+          className="logo"
+          src="https://learntocodetogether.nl/assets/logo.svg"
+        />
+        <div className="title">Created by</div>
+        <div className="subtitle">
+          Rein Op &#x27;t land &amp; Danny van der Jagt
+        </div>
+        <div className="team-container">
+          <img
+            className="team"
+            src="https://learntocodetogether.nl/assets/danny&rein.svg"
+          />
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
